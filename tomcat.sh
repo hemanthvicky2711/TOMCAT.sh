107c50b4e79b78420f8ea48361f1c070ff175761
Hemanth <hemanthkorukonda99@gmail.com>
2026-04-23 07:53:54 +0530

    tomcat.sh script

---
+++ tomcat.sh
@@ -0,0 +1,3 @@
+sudo yum install java-17-amazon-corretto -y
+sudo  wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.117/bin/apache-tomcat-9.0.117.tar.gz
+sudo  tar -zxvf apache-tomcat-9.0.117.tar.gz
