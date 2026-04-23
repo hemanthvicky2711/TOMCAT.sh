428e59a7a67e27ff7e2d18cbed0d89f9572c147f
Hemanth <hemanthkorukonda99@gmail.com>
2026-04-23 08:11:50 +0530

    tomcat.sh script
    Install Java, download and extract Tomcat, and configure user roles.

--- tomcat.sh
+++ tomcat.sh
@@ -1,3 +1,33 @@
+# Run tomcat.sh script in terminal to setup tomcat 
+
 sudo yum install java-17-amazon-corretto -y
 sudo  wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.117/bin/apache-tomcat-9.0.117.tar.gz
 sudo  tar -zxvf apache-tomcat-9.0.117.tar.gz
+
+# go to 
+
+cd apache-tomcat-9.0.117/webapps/manager/META-INF/
+sudo vim context.xml
+
+# Delete line number 21 and 22 : save and exit
+# go to 
+
+ cd apache-tomcat-9.0.117/conf/
+ sudo vim tomcat-users.xml
+
+# Go to line number 50
+#Yank(copy) line number 50,51 & 52
+#Paste the yanked lines in line number 56 and make the changes in role name and password
+
+ <role rolename="manager-gui"/>
+  <role rolename="manager-script"/>
+  <user username="tomcat" password="admin@123" roles="manager-gui,manager-script"/>
+
+# save and exit 
+# Go to 
+
+cd apache-tomcat-9.0.117/bin/
+
+#run 
+
+./startup.sh 
